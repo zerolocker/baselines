56dae6814818f30358ba270143ddcfab2a9c35eb
Zhuode Liu <zhuode.liu@gmail.com>
2018-05-11 12:32:03 -0500

    improve gocondor.py

--- gocondor.py
+++ gocondor.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import sys, re, os, subprocess, numpy as np, string, random, time
 
-def savedir_arg(MODEL_NAME):
-  EXPR_NAME = sys.argv[3]
-  prefix = MODEL_NAME+'_'+EXPR_NAME
-  return '--resumable --save-dir SaveDir/%s_%s_%05d' % (
+def save_model_args(model_name):
+  prefix = model_name+'_'+EXPR_NAME
+  return '--resumable --save-freq 50000 --save-dir SaveDir/%s_%s_%05d' % (
         prefix,time.strftime("%b%d"),random.randint(0,100000))
 
 def create_bgrun_sh_dqnNature_noDoubleQ_model(GAME_NAME):
@@ -12,7 +11,7 @@ def create_bgrun_sh_dqnNature_noDoubleQ_model(GAME_NAME):
   for run_num in range(3):
     sh_file_content += ' '.join(['python3', '-m baselines.deepq.experiments.atari.train',
       '--env', GAME_NAME, '--no-double-q',
-      # savedir_arg('dqn'), # commented because I've not yest tested if deepq/.../train.py works well with --save-dir, but saving model is not very important for now. I'll just leave it commented. Maybe test it in the future.
+      # save_model_args('dqn'), # commented because I've not yest tested if deepq/.../train.py works well with --save-dir, but saving model is not very important for now. I'll just leave it commented. Maybe test it in the future.
        ] + OTHER_PARAMETERS_TO_PASS)
        
     sh_file_content += ' &\n'
@@ -24,11 +23,17 @@ def create_bgrun_sh_DeepqWithGaze_noDoubleQ_model(GAME_NAME):
   for run_num in range(1):
     sh_file_content += ' '.join(['python3', '-m baselines.DeepqWithGaze.experiments.atari.train',
       '--env', GAME_NAME, '--no-double-q',
-      savedir_arg('dqnHgaze'),
+      save_model_args('dqnHgaze'),
       ] + OTHER_PARAMETERS_TO_PASS)
     sh_file_content += ' &\n'
   sh_file_content += 'wait\n'
   return sh_file_content
+
+def fix_wrong_game_name(name):
+  name = str.capitalize(name)
+  if (name.lower() == 'mspacman'):
+    name = "MsPacman"
+  return name
 
 ALL_GAME_NAMES=[
    ("Breakout"),
@@ -41,7 +46,32 @@ ALL_GAME_NAMES=[
    ("Venture"),
 ]
 
-def main(bg_run_creator_func):
+MODEL_SH_MAPPING = {
+        "dqnNature_noDoubleQ": create_bgrun_sh_dqnNature_noDoubleQ_model,
+        "DeepqWithGaze_noDoubleQ": create_bgrun_sh_DeepqWithGaze_noDoubleQ_model
+        }
+
+OTHER_PARAMETERS_TO_PASS = sys.argv[4:] # TODO: refactor this and be less hacky
+EXPR_NAME = sys.argv[3]
+CHOSEN = [sys.argv[1]] if sys.argv[1] != 'all' else ALL_GAME_NAMES
+SH_FILE_DIR =  os.path.abspath('bgrun_yard')
+
+if len(sys.argv) < 4:
+    print("Usage: %s <GAME_NAME|all> <MODEL_NAME> <YOUR_EXPR_NAME> <OTHER_PARAMETERS_TO_PASS>" % __file__)
+    print("'all' means run all games:", ALL_GAME_NAMES)
+    print("Supported MODEL_NAME are: " , MODEL_SH_MAPPING.keys())
+    sys.exit(1)
+
+if sys.argv[2] not in MODEL_SH_MAPPING:
+    print("ERROR: Wrong model name.")
+    print("Supported model names are: " , MODEL_SH_MAPPING.keys())
+    sys.exit(0)
+else:
+    bg_run_creator_func = MODEL_SH_MAPPING[sys.argv[2]]
+    print( "Job output will be directed to folder ./CondorOutput")
+    if not os.path.exists("CondorOutput"):
+      os.mkdir("CondorOutput")
+    
     basestr="""
     # doc at : http://research.cs.wisc.edu/htcondor/manual/current/condor_submit.html
     arguments = {0}
@@ -64,18 +94,8 @@ def main(bg_run_creator_func):
     Queue
     """
 
-    def fix_wrong_game_name(name):
-      name = str.capitalize(name)
-      if (name.lower() == 'mspacman'):
-        name = "MsPacman"
-      return name
-
-    SH_FILE_DIR =  os.path.abspath('bgrun_yard')
-    EXPR_NAME = sys.argv[3]
-
     if not os.path.exists(SH_FILE_DIR):
       os.makedirs(SH_FILE_DIR)
-    CHOSEN = [sys.argv[1]] if sys.argv[1] != 'all' else ALL_GAME_NAMES
     for GAME_NAME in CHOSEN:
         sh_file_content = bg_run_creator_func(fix_wrong_game_name(GAME_NAME))
         print(sh_file_content)
@@ -85,31 +105,8 @@ def main(bg_run_creator_func):
         sh_f = open(sh_filename, 'w')
         sh_f.write(sh_file_content)
 
-        submission = basestr.format(sh_filename, EXPR_NAME)
         with open('submit.condor', 'w') as f:
-          f.write(submission)
+          f.write(basestr.format(sh_filename, EXPR_NAME))
 
         subprocess.call(['condor_submit', 'submit.condor'])
-
-model_to_func = {
-        "dqnNature_noDoubleQ": create_bgrun_sh_dqnNature_noDoubleQ_model,
-        "DeepqWithGaze_noDoubleQ": create_bgrun_sh_DeepqWithGaze_noDoubleQ_model
-        }
-
-if len(sys.argv) < 4:
-  print("Usage: %s <GAME_NAME|all> <MODEL_NAME> <YOUR_EXPR_NAME> <OTHER_PARAMETERS_TO_PASS>" % __file__)
-  print("'all' means run all games:", ALL_GAME_NAMES)
-  print("Supported MODEL_NAME are: " , model_to_func.keys())
-  sys.exit(1)
-
-OTHER_PARAMETERS_TO_PASS = sys.argv[4:] # TODO: refactor this and be less hacky
-if sys.argv[2] in model_to_func:
-    print( "Job output will be directed to folder ./CondorOutput")
-    if not os.path.exists("CondorOutput"):
-      os.mkdir("CondorOutput")
-    main(model_to_func[sys.argv[2]])
-else:
-    print("ERROR: Wrong model name.")
-    print("Supported model names are: " , model_to_func.keys())
-    sys.exit(0)
 
