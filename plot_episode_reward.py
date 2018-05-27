5acea2624d3daf7b22ec927297db719c711e2348
Zhuode Liu <zhuode.liu@gmail.com>
2018-05-27 13:46:11 -0500

    enable plot_episode_reward.py to accept path pattern

--- plot_episode_reward.py
+++ plot_episode_reward.py
@@ -5,31 +5,51 @@ from matplotlib.pyplot import cm
 import argparse, re
 from collections import defaultdict
 import numpy as np
-import os
+import os, subprocess, sys
 
 def main():
-  parser = argparse.ArgumentParser('You must specify a (model) name for each file. ' +
-          'e.g. 23456.out Dqn 23486.out DqnWithGaze ... \n' + 
-    ' np.max() is used to aggregate rewards in a episode.')
-  parser.add_argument('file_and_name', metavar='file_and_name_seperated_by_a_space', nargs = '+')
+  parser = argparse.ArgumentParser('You must specify a model name for each file/file pattern. ' +
+    'e.g. 23456.out Dqn 23486.out DqnWithGaze\n' + 
+    '[path pattern is supported, in `grep` syntax]\n' +
+    'e.g. dqnHgaze_freezeGaze.*/log.txt freezeGaze dqnHgaze_trainableGaze.*/log.txt trainableGaze \n' +
+    'If there are >1 file becasue you used path pattern, rewards belonging to the same episode is aggregrated by np.max().')
+  parser.add_argument('files_and_modelnames', metavar='files_and_modelnames_seperated_by_a_space', nargs = '+')
   args = parser.parse_args()
-  assert(len(args.file_and_name)%2==0)
-  n = len(args.file_and_name)
-  files = args.file_and_name[0:n:2]
-  names =  args.file_and_name[1:n:2]
+  assert(len(args.files_and_modelnames)%2==0)
+  n = len(args.files_and_modelnames)
+  files_pattern = args.files_and_modelnames[0:n:2]
+  modelnames =  args.files_and_modelnames[1:n:2]
+
+  # concatenate all log files into a python list, grouped by model name
+  concated_log = []
+  for (pattern, modelname) in zip(files_pattern, modelnames):
+    try:
+      matched_files = subprocess.check_output("find . | grep '%s'" % pattern, shell=True).decode('utf-8')
+    except subprocess.CalledProcessError as ex:
+      print(ex)
+      print("Regex might be wrong (for example, did you use * instead of .* ?)")
+      sys.exit(1)
+    print("modelname: '%s' pattern: '%s' matched the following files:" % (modelname, pattern))
+    print(matched_files)
+    matched_files = matched_files.split()
+    concated_log.append([])
+    for fname in matched_files:
+      assert "ASCII" in subprocess.check_output(["file", fname]).decode('utf-8'), "This file is not ASCII file: " + fname
+      with open(fname, 'r') as f:
+        concated_log[-1] += f.readlines() # readlines() returns a list
 
   color = iter(cm.rainbow(np.linspace(0,1,n/2)))
-  for (file, name) in zip(files, names):
-    myplot(file, next(color), name)
+  for (log, modelname) in zip(concated_log, modelnames):
+    myplot(log, next(color), modelname)
   finalize_plot_and_show()
 
-def myplot(file, color, name):
-  EPI_REGEX = re.compile('\| episodes              \| (\d+)')
-  REWARD_REGEX = re.compile('\| reward \(100 epi mean\) \| ([\d\.e\+\-]+)')
+def myplot(log, color, modelname):
+  EPI_REGEX = re.compile(r'\| episodes              \| (\d+)')
+  REWARD_REGEX = re.compile(r'\| reward \(100 epi mean\) \| ([\d\.e\+\-]+)')
 
   epi_data = defaultdict(list)
   episode, reward = None, None
-  for line in open(file,'r'):
+  for line in log:
     if EPI_REGEX.match(line):
       episode = int(EPI_REGEX.match(line).group(1))
     if REWARD_REGEX.match(line):
@@ -40,7 +60,7 @@ def myplot(file, color, name):
   for (epi, rew_list) in epi_data.items():
     x.append(epi)
     y.append(np.max(rew_list))
-  plt.plot(x,y, c=color, label=name)
+  plt.plot(x,y, c=color, label=modelname)
 
 def finalize_plot_and_show():
   plt.xlabel("Episode")
